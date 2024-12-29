4329fbe129e8e4f5a393f96623f8cdb1121170f0
oversight007 <oversight18@gmail.com>
2024-12-29 17:15:22 -0500

    Update index.ts

--- TinfoilTaskforce/src/index.ts
+++ TinfoilTaskforce/src/index.ts
@@ -1,4 +1,19 @@
 import { CindyAgent } from "./agents/CindyAgent";
+import { TimmyAgent } from "./agents/TimmyAgent";
+import { DebbieAgent } from "./agents/DebbieAgent";
+import { AndyAgent } from "./agents/AndyAgent";
+import { BobAgent } from "./agents/BobAgent";
+import { AnnieAgent } from "./agents/AnnieAgent";
 
-const cindy = new CindyAgent();
-console.log(cindy.generatePost());
+const agents = [
+  new CindyAgent(),
+  new TimmyAgent(),
+  new DebbieAgent(),
+  new AndyAgent(),
+  new BobAgent(),
+  new AnnieAgent()
+];
+
+agents.forEach(agent => {
+  console.log(agent.generatePost());
+});
